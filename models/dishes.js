cbe54f4672e391b635e336239eefa1cc953b622f
AbhinavThombare <abhinavthombare23@gmail.com>
2023-01-05 18:56:05 +0530

    Created update api for dishes

--- models/dishes.js
+++ models/dishes.js
@@ -2,39 +2,38 @@ const mongoose = require('mongoose')
 
 const DishesSchema = new mongoose.Schema({
     Category: {
-        type:String,
-        required:true,
-        trim:true
+        type: String,
+        required: true,
+        trim: true
     },
     Dish_Name: {
-        type:String,
-        required:true,
-        trim:true
+        type: String,
+        required: true,
+        trim: true
 
     },
     Price: {
-        type:String,
-        required:true,
-        trim:true
+        type: String,
+        required: true,
+        trim: true
 
     }
 })
 
-DishesSchema.methods.addDish =async function() {
-    const dish = this;
-    const name = dish.Dish_Name
-    console.log(dish.Dish_Name)
-    const dishdatabase = await Dish.findOne({Dish_Name:name})
-    console.log(dishdatabase)
-    if(dishdatabase){
-        return Error({message:'Dish Already Exists!'})
+DishesSchema.methods.addDish = async function () {
+    try {
+        const dish = this;
+        
+        // console.log(dish)
+        // await dish.save()
+        // return dish;
+        return dish
+    } catch (error) {
+        return error
     }
-    // console.log(dish)
-    // await dish.save()
-    // return dish;
 }
 
-const Dish = mongoose.model('Dish',DishesSchema)
+const Dish = mongoose.model('Dish', DishesSchema)
 
 
 module.exports = Dish;
--- routes/dishRoutes.js
+++ routes/dishRoutes.js
@@ -4,27 +4,41 @@ const Dish = require('../models/dishes')
 
 
 router.post('/api/dish/add', async (req, res) => {
-    const dish = new Dish(req.body.dish)
-
     try {
-        const returnDish = await dish.addDish()
-        if(!returnDish){
-            return res.status(400).send('Dish Not Added')
+        // const returnDish = await dish.addDish()
+        const dish = new Dish(req.body.dish)
+        const dishdatabase = await Dish.findOne({ Dish_Name: dish.Dish_Name })
+
+        if (dishdatabase) {
+            throw Error('Dish Already Exists!')
         }
-        // console.log(returnDish)
+        await dish.save();
         return res.status(200).send()
     } catch (error) {
-        console.log(error)
-        return res.status(400).send(error)
+        return res.status(400).send(error.message)
     }
 })
 
 router.get('/api/dish/alldisges',async(req,res) => {
     const dishes =await Dish.find({})
-    console.log(dishes)
+    // console.log(dishes)
 
     return res.status(200).send({dishes})
 })
 
+router.put('/api/dish/updatedish',async(req,res) => {
+    const updatedish = Dish(req.body.dish)
+    console.log(updatedish._id)
+    const dish = await Dish.findOneAndUpdate({_id:updatedish._id},{
+        Category:updatedish.Category,
+        Dish_Name:updatedish.Dish_Name,
+        Price:updatedish.Price
+    })
+    
+
+    console.log(dish)
+    await dish.save()
+    return res.status(200).send()
+})
 
 module.exports = router
